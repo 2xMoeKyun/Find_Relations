d0e4bcb6c541eebd49d242fb6fba5740546ad19c
ANullchance <92113036+ANullchance@users.noreply.github.com>
2022-11-17 22:33:27 +0300

    Data base location change

--- FindRel/Data base/SQLQueryForAttach.sql
+++
@@ -1,5 +0,0 @@
-   CREATE DATABASE ProductsDB ON 
-                (FILENAME = N'C:\Users\gasim\Desktop\Ñ# Projects\Find_Relations\FindRel\Data base\ProductsDB.mdf'),
-                (FILENAME = N'C:\Users\gasim\Desktop\Ñ# Projects\Find_Relations\FindRel\Data base\ProductsDB_log.ldf')
-   FOR ATTACH
-   GO
